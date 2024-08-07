9a782d8b9ae5979371cd79e2c49bfc0590fce796
devaev.lev <ldevaev@mail.ru>
2024-08-07 09:19:34 +0500

    убрано кастомное логирование, добавил встроенное логирование pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,12 @@
 import os
+import sys
 
-import logging
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-from settings import BASE_DIR
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
 
 chrome_options = Options()
 download_dir = os.path.join(BASE_DIR, 'tests')
@@ -16,30 +17,14 @@ chrome_options.add_experimental_option('prefs', {
     'safebrowsing.enabled': True
 })
 
-@pytest.fixture(scope='session')
-def logger():
-    logger = logging.getLogger("LOG:")
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    logger.setLevel(logging.INFO)
-    return logger
-
 
 @pytest.fixture(scope="module")
-def browser(logger):
-    logger.info("Инициализация браузера Chrome")
+def browser():
     browser = webdriver.Chrome(options=chrome_options)
     browser.implicitly_wait(10)
     yield browser
-    logger.info("Закрытие браузера")
 
     browser.quit()
     file_path = os.path.join(BASE_DIR, 'tests/sbisplugin-setup-web.exe')
     if os.path.exists(file_path):
         os.remove(file_path)
-        logger.info(f"Удаление файла {file_path}")
-    else:
-        logger.warning(f"Файл {file_path} не найден для удаления")
-
